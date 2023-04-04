b064c9c784a8723466a41ab967fa3045b52c82b1
Jon Canning <jon.canning@gmail.com>
2023-04-04 11:36:38 +0100

    bump version

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
-NUGETVERSION=1.0.5
+NUGETVERSION=1.0.6
 dotnet pack src/MongoDBQ.csproj -c Release /p:PackageVersion=$NUGETVERSION
 dotnet nuget push src/bin/Release/MongoDBQ.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
