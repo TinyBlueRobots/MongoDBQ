0bf267f67186f624fb9ec7049003046399a510f4
Jon Canning <jon.canning@gmail.com>
2023-09-08 17:28:48 +0100

    add demo app

--- sln.sh
+++ sln.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
 rm *.sln
 dotnet new sln
-find "src" -name "*proj" | xargs -I {} dotnet sln add {}
-find "tests" -name "*proj" | xargs -I {} dotnet sln add {}
+find -name "*proj" | xargs -I {} dotnet sln add {}
